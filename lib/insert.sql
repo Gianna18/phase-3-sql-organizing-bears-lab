6bad92d660294ef2d634120d20b02594d979eef3
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-02-18 11:30:55 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr.Chocolate", 12, "M", "red", "cool", true ),
+       ("Rowdy", 16, "M", "black", "wild", false),
+       ("Tabitha", 20, "F", "white", "calm", true),
+       ("Sergeant Brown", 24, "M", "blue", "cautious", true),
+       ("Melissa", 28, "F", "pink", "party animal", false),
+       ("Grinch", 32, "M", "yellow", "Not cool", false),
+       ("Wendy", 36, "F", "maroon", "Thoughtful", true),
+       (NULL, 6, "M","Crimson", "Reserved", false);
